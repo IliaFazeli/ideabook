e6a51a709dd195861b2a780099e1b8d3ce8328ff
shanejbb <shanejabbari@gmail.com>
2020-08-02 16:46:38 +0430

    Firebase Backend integrated

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ideabook/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Return either home or auth widget based on user's status
+
+    return Home();
+  }
+}
